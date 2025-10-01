40dd8b80c6359d36977038c2d477f8f7be2671e7
AnnMinNyo <nyoannmin12@gmail.com>
2025-10-01 16:20:29 +0630

    lab3a final

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -4,15 +4,22 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        // Create new Application
         App app = new App();
+
+        // Connect to database
         app.connect();
 
-        // Example: get all Engineers
-        ArrayList<Employee> engineers = app.getEmployeesByTitle("Engineer");
+        // --- Part 1: Get all salaries ---
+        ArrayList<Employee> allEmployees = app.getAllSalaries();
+        System.out.println("Total employees: " + allEmployees.size());
 
-        // Display results
+        // --- Part 2: Get all Engineers ---
+        ArrayList<Employee> engineers = app.getEmployeesByTitle("Engineer");
+        System.out.println("Engineers:");
         app.displayEmployees(engineers);
 
+        // Disconnect from database
         app.disconnect();
     }
 }
